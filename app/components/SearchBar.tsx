249f9c4a53d0729330ad26d11a170ea3907b6b56
Jakub Malinowski <jmalinowski712@gmail.com>
2025-06-13 16:53:10 +0200

    maps working

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function SearchBar() {
+    return (
+        <>
+            <div className="absolute w-md h-sm bg-white rounded-[14px]">
+                <h1>HELLO</h1>
+            </div>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,82 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { useEffect, useState } from 'react'
+import {APIProvider, Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
+import Image from 'next/image'
+import blueDot from '../public/icons/blueBlinkingDot.svg'
+
+import SearchBar from './components/SearchBar';
+
+interface Coords {
+  lat: number,
+  lng: number
+}
+
+export default function MapPage() {
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string
+
+  const [mapCenter, setMapCenter] = useState<Coords>({ lat: 52.2297, lng: 21.0122 })
+  const [userLocation, setUserLocation] = useState<Coords | null>(null)
+
+  let watchId: number
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      watchId = navigator.geolocation.watchPosition(
+          // success
+          (position) => {
+            const { latitude, longitude } = position.coords
+            const newLocation: Coords = { lat: latitude, lng: longitude }
+
+            setUserLocation(newLocation)
+            setMapCenter(newLocation)
+          },
+          // error
+          (error) => {
+            console.log(error.message)
+          },
+          // options for watchPosition()
+          {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 10000
+          }
+      )
+    } else {
+      // in case if browser does not support Geolocation API
+      const browserSupportError = 'Twoja przeglądarka nie obsługuje Geolocation API.';
+      console.error(browserSupportError);
+      setMapCenter({ lat: 52.2297, lng: 21.0122 });
+    }
+
+    return () => navigator.geolocation.clearWatch(watchId)
+  }, [])
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <APIProvider apiKey={apiKey}>
+          <Map
+            style={{width: '100dvw', height: '100dvh'}}
+            center={mapCenter}
+            defaultZoom={17}
+            disableDefaultUI={true}
+            mapId='3b51350f1553c5e4b1f29045'
+          >
+            <AdvancedMarker
+              position={userLocation}
+              title="UserLocation"
+            >
+              <Image 
+                src={blueDot}
+                width={48}
+                height={48}
+                alt="Blinking Blue Dot Marker"
+              />
+            </AdvancedMarker>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+            <SearchBar />
+          </Map>
+      </APIProvider>
+    </>
+  )
 }
